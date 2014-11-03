d920124cee9569d539df8dfd48622d4c300a54b4
Jahde <jahde.hiywan@gmail.com>
2014-11-03 14:47:31 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Halloween2::Application.config.secret_key_base = 'b3c4734ba3325c0dd6bc08add40d9c26ead57ea0cfce4b1a3bc904dcb2c5e31d92919542f1b25c71df42dea47b07cce2a01eb2a73f51a529486e4fa40bdaf2ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Halloween2::Application.config.session_store :cookie_store, key: '_halloween2_session'
